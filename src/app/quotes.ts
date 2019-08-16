477f510d2154a873c17c26f0728390cb8e15fe1b
Nyota254 <knyota66@gmail.com>
2019-08-16 14:20:17 +0300

    Creation of the class which is a blueprint for quotes objects and Html docs for the goal component

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,2 +1,6 @@
 export class Quotes {
+     showDetails: boolean;
+     constructor(public name:string , public quote:string, public author:string, public votes:number, public dateEnterd:Date){
+        this.showDetails = false;
+     }
 }
