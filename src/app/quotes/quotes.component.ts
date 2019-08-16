065ed0385ac3f2c7ee10e2df0872826a993195fb
Nyota254 <knyota66@gmail.com>
2019-08-16 15:03:56 +0300

    initialise the quotes object and create an array of objects and also modify the html file to show the quotes also add bootsrap

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quotes } from '../quotes'
 
 @Component({
   selector: 'app-quotes',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
+
+  quotes : Quotes[] = [
+    new Quotes('Pulisic','Live as if you were to die tomorrow. Learn as if you were to live forever' , 'Mahatma Gandhi' , 0 , new Date(2019,2,1)),
+    new Quotes('Pedro','Be who you are and say what you feel, because those who mind don’t matter and those who matter don’t mind.' , 'Bernard M. Baruch' , 0 , new Date(2019,3,1)),
+    new Quotes('Kante','If you cannot do great things, do small things in a great way.' , 'Napoleon Hill' , 0 , new Date(2019,4,1)),
+    new Quotes('Giroud','Strive not to be a success, but rather to be of value.' , 'Albert Einstein' , 0 , new Date(2019,5,1)),
+    new Quotes('Frank Lampard','I haven’t failed. I’ve just found 10,000 ways that won’t work.' , 'Thomas Edison' , 0 , new Date(2019,6,1))
+  ]
 
   constructor() { }
 
